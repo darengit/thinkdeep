44fcad8e88835e3fef120760b3389ca5b24c5ebd
dzou <dzou@appnexus.com>
2015-02-17 11:30:44 -0500

    continue on error with urllib.request.urlopen

--- yahoo_finance.py
+++ yahoo_finance.py
@@ -9,6 +9,9 @@ url = "http://finance.yahoo.com/q?s=" + ticker
 
 
 while True:
-    print((re.search('<span id="yfs_l10_' + ticker.replace("^", "\^") + '">([0-9,\.]*)</span>', list(urllib.request.urlopen(url))[180].decode("utf-8"))).group(1))
+    try:
+        print((re.search('<span id="yfs_l10_' + ticker.replace("^", "\^") + '">([0-9,\.]*)</span>', list(urllib.request.urlopen(url))[180].decode("utf-8"))).group(1))
+    except:
+        continue
     time.sleep(2)
 
